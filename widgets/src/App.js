a3afd9b26de75dc0216e783cf52bfd8d2b6c74fc
= <=>
2020-10-01 23:50:32 -0400

    Adding accordion component, implementing usestate hook

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,9 +1,27 @@
 import React from "react";
+import Accordion from "./components/Accordion";
 
-class App extends React.Component {
-  render() {
-    return <h1>Widgets App</h1>;
-  }
-}
+const items = [
+  {
+    title: "What is React?",
+    content: "React is a front end javascript framework",
+  },
+  {
+    title: "Why use React?",
+    content: "React is a favorite JS library among engineers",
+  },
+  {
+    title: "How do you use React?",
+    content: "You use React by creating components",
+  },
+];
 
-export default <App />;
+const App = () => {
+  return (
+    <div>
+      <Accordion items={items} />
+    </div>
+  );
+};
+
+export default App;
