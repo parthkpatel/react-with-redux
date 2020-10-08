ae3bbc514fdd75f8313945c911888365510bd9bf
= <=>
2020-10-08 15:07:14 -0400

    Completing blog react app with redux

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,9 +1,28 @@
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+export const fetchPostsAndUsers = () => {
+  return async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+    const userIds = _.uniq(_.map(getState().posts, "userId"));
+    userIds.forEach((id) => {
+      dispatch(fetchUser(id));
+    });
+  };
+};
 
 export const fetchPosts = () => {
   return async (dispatch) => {
     const response = await jsonPlaceholder.get("/posts");
 
-    dispatch({ type: "FETCH_POSTS", payload: response });
+    dispatch({ type: "FETCH_POSTS", payload: response.data });
+  };
+};
+
+export const fetchUser = (id) => {
+  return async (dispatch) => {
+    const response = await jsonPlaceholder.get(`users/${id}`);
+
+    dispatch({ type: "FETCH_USER", payload: response.data });
   };
 };
--- blog/src/reducers/index.js
+++ blog/src/reducers/index.js
@@ -1,3 +1,8 @@
 import { combineReducers } from "redux";
+import postsReducer from "./postsReducer";
+import usersReducer from "./usersReducer";
 
-export default combineReducers({ replaceMe: () => "hi there" });
+export default combineReducers({
+  posts: postsReducer,
+  users: usersReducer,
+});
