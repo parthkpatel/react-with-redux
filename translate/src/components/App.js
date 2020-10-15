100735bf6340ab027fe2fcb6dddadfdcf52014eb
= <=>
2020-10-14 21:30:55 -0400

    Adding Button and Field components, using React context system instead of props system

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React from "react";
+import UserCreate from "./UserCreate";
+import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
 
 class App extends React.Component {
-    render() {
-        return (
-            <div className="ui container">App</div>
-        )
-    }
+  state = { language: "english" };
+
+  onLanguageChange = (language) => {
+    this.setState({ language: language });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <div>
+          Select a language
+          <i
+            className="flag us"
+            onClick={() => this.onLanguageChange("english")}
+          />
+          <i
+            className="flag nl"
+            onClick={() => this.onLanguageChange("dutch")}
+          />
+        </div>
+        <ColorContext.Provider value="primary">
+          <LanguageContext.Provider value={this.state.language}>
+            <UserCreate />
+          </LanguageContext.Provider>
+        </ColorContext.Provider>
+      </div>
+    );
+  }
 }
 
 export default App;
